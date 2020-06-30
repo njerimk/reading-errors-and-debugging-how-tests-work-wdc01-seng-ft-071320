6a79858098b236b8fdd6bc2d3733403f6f06278c
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-06-30 15:29:44 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,4 +3,4 @@ first_number=81
 second_number = 9
 sum = first_number + second_number
 difference= first_number - second_number
-product=first_number-second_number 
+product=first_number*second_number 
